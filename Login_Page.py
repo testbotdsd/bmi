fb6468dc3a2cd72d6f423adf3dd4c9d914cb0e76
Angelaineee <sampangangelaine@gmail.com>
2024-03-14 14:07:22 +0800

    asd

--- Login_Page.py
+++ Login_Page.py
@@ -6,11 +6,19 @@ class Login (tk.Frame):
         self.parent = master
         self.config(width=400, height=600)
         
+
+        #ANGELAINE NEW
+        self.welcome_label = tk.Label(self, text="WELCOME", font="Arial 20 bold")
+        self.welcome_label.place(x=0, y=0)
+
+        self.email_entry = tk.Entry(self, border=1)
+        self.email_entry.place(x=10, y=50)
+
         self.back_button = tk.Button(self, text="back", command=self.go_to_welcome_page)
-        self.back_button.place(x=30, y=30)
+        self.back_button.place(x=30, y=550)
 
         self.continue_button = tk.Button(self, text='Login', command=self.go_to_BMI_Page)
-        self.continue_button.place(x=70, y=30)
+        self.continue_button.place(x=70, y=550)
 
 
     def go_to_welcome_page(self):
