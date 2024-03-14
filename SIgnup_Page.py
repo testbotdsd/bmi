70f7d71b84cf051100c3ec2adb1b8e96f825ce9e
Jeromeee10 <markjerome.rosana@gmail.com>
2024-03-14 13:54:40 +0800

    jijijij

---
+++ SIgnup_Page.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+class Signup(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__ (self, master)
+        self.parent = master
+        self.config(width=400, height=600)
+
+        self.back_button = tk.Button(self, text="back", command=self.go_to_welcome_page)
+        self.back_button.place(x=30, y=30)
+
+        self.continue_button = tk.Button(self, text='Login', command=self.go_to_Login_Page)
+        self.continue_button.place(x=70, y=30)
+
+
+    def go_to_welcome_page(self):
+        self.parent.change_window('Welcome_Page')
+
+
+    def go_to_Login_Page(self):
+        self.parent.change_window('Login')
+
+
+    def on_return(self):
+        pass
--- Welcome_Page.py
+++ Welcome_Page.py
@@ -9,24 +9,19 @@ class Welcome(tk.Frame):
         self.login_bg = tk.Frame(self, bg='#4F4A45', height=400, width=350)
         self.login_bg.place(x=0, y=0)
 
-        # self.background_image = tk.PhotoImage(file="login picture.png")
-        # self.resized_background_image = self.background_image.subsample(2,2)
-        # self.background_label = tk.Label(self, image=self.resized_background_image)
-        # self.background_label.place(relwidth=1, relheight=1)
-
-        self.welcome1_label = tk.Label(self, text="WELCOME TO", font="Arial 25 bold", bg='#4F4A45', 
-                                      foreground='#ED7D31')
+        self.welcome1_label = tk.Label(self, text="WELCOME TO", font="Arial 25 bold", bg='#4F4A45', foreground='#ED7D31')
         self.welcome1_label.place(x=57, y=40)
-        self.welcome2_label = tk.Label(self, text="BMI CALCULATOR", font="Arial 25 bold", bg='#4F4A45', 
-                                      foreground='#ED7D31')
+        self.welcome2_label = tk.Label(self, text="BMI CALCULATOR", font="Arial 25 bold", bg='#4F4A45', foreground='#ED7D31')
         self.welcome2_label.place(x=20, y=80)
 
         self.login_btn = tk.Button(self, text="Login", font="Garamond 12 bold", width=12, command=self.go_to_login_page)
         self.login_btn.place(x=100, y=195)
 
-        self.sign_up_btn = tk.Button(self, text="Signup", font="Garamond 12 bold", width=12)
+        self.sign_up_btn = tk.Button(self, text="Signup", font="Garamond 12 bold", width=12, command=self.go_to_Sign_up_Page)
         self.sign_up_btn.place(x=100, y=235)
         
+    def go_to_Sign_up_Page(self):
+        self.master.change_window('Signup')
 
     def go_to_login_page(self):
         self.master.change_window('Login')
