e2f54de14d04b05ddfcb0df208e835da774675e0
Angelaineee <sampangangelaine@gmail.com>
2024-03-14 20:13:58 +0800

    angel

--- Login_Page.py
+++ Login_Page.py
@@ -10,7 +10,8 @@ class Login (tk.Frame):
         self.login_bg = tk.Frame(self, bg='#7B6079', height=595, width=450)
         self.login_bg.place(x=0, y=0)
 
-        self.welcome_label = tk.Label(self.login_bg, text="WELCOME", font=('Courier', 45, 'bold'), fg='white', bg='#7B6079')
+        self.welcome_label = tk.Label(self.login_bg, text="WELCOME", font=('Courier', 45, 'bold'), fg='white', 
+                                      bg='#7B6079')
         self.welcome_label.place(x=95, y=40)
 
         self.email_label = tk.Label(self.login_bg, text="Username", font=('Courier', 15), fg='white', bg='#7B6079')
@@ -25,19 +26,23 @@ class Login (tk.Frame):
         self.pass_entry = tk.Entry(self.login_bg, border=1, width=20, font=('Courier', 15), bg='#694e67')
         self.pass_entry.place(x=100, y=255)
 
-        self.back_button = tk.Button(self.login_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'),bg='#FFE9D6',command=self.go_to_welcome_page)
+        self.back_button = tk.Button(self.login_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'),
+                                     bg='#FFE9D6',command=self.go_to_welcome_page)
         self.back_button.place(x=5, y=10)
 
-        self.login_button = tk.Button(self.login_bg, text='LOGIN', width=19, font=('Courier', 15, 'bold'),bg='#DE8971',command=self.go_to_BMI_Page)
+        self.login_button = tk.Button(self.login_bg, text='LOGIN', width=19, font=('Courier', 15, 'bold'),bg='#DE8971',
+                                      command=self.go_to_BMI_Page)
         self.login_button.place(x=100, y=444)
 
-        self.forgot_password_button = tk.Button(self.login_bg, text = 'Forgot your password?',  font=('Courier', 11), fg='#88f2ea', bg='#7B6079',bd = 0, command=self.go_to_forgot_password)
+        self.forgot_password_button = tk.Button(self.login_bg, text = 'Forgot your password?',  font=('Courier', 11), 
+                                                fg='#88f2ea', bg='#7B6079',bd = 0, command=self.go_to_forgot_password)
         self.forgot_password_button.place (x=153, y=282)
 
         self.no_account_label = tk.Label(self, text="Don't have an account?", bg='#7B6079', font="Courier 8")
         self.no_account_label.place(x=99, y=488)
 
-        self.sign_up_btn = tk.Button(self, text="Sign up now", bg='#7B6079', bd=0, font=('Courier', 8, 'bold'), command=self.go_to_signup, foreground='#88f2ea')
+        self.sign_up_btn = tk.Button(self, text="Sign up now", bg='#7B6079', bd=0, font=('Courier', 8, 'bold'), 
+                                     command=self.go_to_signup, foreground='#88f2ea')
         self.sign_up_btn.place(x=259, y=488)
 
     def go_to_forgot_password(self):
@@ -61,10 +66,12 @@ class Forgot_Password (tk.Frame):
         self.login_bg = tk.Frame(self, bg='#7B6079', height=595, width=450)
         self.login_bg.place(x=0, y=0)
 
-        self.welcome_label = tk.Label(self.login_bg, text="FORGOT PASSWORD", font=('Courier', 20, 'bold'), fg='white', bg='#7B6079')
+        self.welcome_label = tk.Label(self.login_bg, text="FORGOT PASSWORD", font=('Courier', 20, 'bold'), fg='white', 
+                                      bg='#7B6079')
         self.welcome_label.place(x=95, y=40)
 
-        self.back_button = tk.Button(self.login_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'),bg='#FFE9D6')
+        self.back_button = tk.Button(self.login_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'),
+                                     bg='#FFE9D6')
         self.back_button.place(x=5, y=10)
 
     def go_to_login(self):
--- SIgnup_Page.py
+++ SIgnup_Page.py
@@ -6,58 +6,62 @@ class Signup(tk.Frame):
         self.parent = master
         self.config(width=400, height=600)
 
+        #WELCOME LABELS
+
         self.create_account_bg = tk.Frame(self, bg='#7B6079', height=600, width=400)
         self.create_account_bg.place(x=0, y=0)
 
         self.create_account_label = tk.Label(self, text='Create an Account', font=('Courier', 25, 'bold'), 
                                              bg='#7B6079', foreground="white")
-        self.create_account_label.place(x=10, y=10)
+        self.create_account_label.place(x=25, y=10)
 
         self.to_get_started_label = tk.Label(self, text='to get started', font=('Courier', 15, 'bold'),  
                                              bg='#7B6079', foreground="white")
-        self.to_get_started_label.place(x=130, y=40)
+        self.to_get_started_label.place(x=121, y=40)
+
+        #INFORMATIONS
+        self.first_name_label = tk.Label(self, text='First Name', font=('Courier', 10), fg='white', bg='#7B6079')
+        self.first_name_label.place(x=10, y=100)
+
+        self.first_name_entry = tk.Entry(self, border=1)
+        self.first_name_entry.place(x=80, y=118)
+
+        self.last_name_label = tk.Label(self, text='Last Name', font=('Courier', 10), fg='white', bg='#7B6079')
+        self.last_name_label.place(x=10, y=135)
+
+        self.last_name_entry = tk.Entry(self, border=1)
+        self.last_name_entry.place(x=80, y=153)
+
+        self.gmail_label = tk.Label(self, text='Gmail', font=('Courier', 10), fg='white', bg='#7B6079')
+        self.gmail_label.place(x=10, y=170)
+
+        self.gmail_entry = tk.Entry(self, border=1)
+        self.gmail_entry.place(x=55, y=189)
+
+        self.username_label = tk.Label(self, text='Username', font=('Courier', 10), fg='white', bg='#7B6079')
+        self.username_label.place(x=10, y=205)
+
+        self.username_entry = tk.Entry(self, border=1)
+        self.username_entry.place(x=80, y=222)
+
+        self.password_label = tk.Label(self, text='Password', font=('Courier', 10), fg='white', bg='#7B6079')
+        self.password_label.place(x=10, y=240)
+
+        self.password_entry = tk.Entry(self, border=1)
+        self.password_entry.place(x=75, y=259)
+
+        self.confirm_password_label = tk.Label(self, text='Confirm Password', font=('Courier', 10), fg='white', 
+                                               bg='#7B6079')
+        self.confirm_password_label.place(x=10, y=275)
+
+        self.confirm_password_entry = tk.Entry(self, border=1)
+        self.confirm_password_entry.place(x=125, y=194)
 
         self.back_button = tk.Button(self, text="back", command=self.go_to_welcome_page)
         self.back_button.place(x=30, y=550)
 
         self.continue_button = tk.Button(self, text='Login', command=self.go_to_Login_Page)
         self.continue_button.place(x=70, y=550)
-
-        self.first_name_label = tk.Label(self, text='First Name')
-        self.first_name_label.place(x=10, y=100)
-
-        self.first_name_entry = tk.Entry(self, border=1)
-        self.first_name_entry.place(x=80, y=100)
-
-        self.last_name_label = tk.Label(self, text='Last Name')
-        self.last_name_label.place(x=10, y=135)
-
-        self.last_name_entry = tk.Entry(self, border=1)
-        self.last_name_entry.place(x=80, y=135)
-
-        self.gmail_label = tk.Label(self, text='Gmail')
-        self.gmail_label.place(x=10, y=170)
-
-        self.gmail_entry = tk.Entry(self, border=1)
-        self.gmail_entry.place(x=55, y=170)
-
-        self.username_label = tk.Label(self, text='Username')
-        self.username_label.place(x=10, y=205)
-
-        self.username_entry = tk.Entry(self, border=1)
-        self.username_entry.place(x=80, y=205)
-
-        self.password_label = tk.Label(self, text='Password')
-        self.password_label.place(x=10, y=240)
-
-        self.password_entry = tk.Entry(self, border=1)
-        self.password_entry.place(x=75, y=240)
-
-        self.confirm_password_label = tk.Label(self, text='Confirm Password')
-        self.confirm_password_label.place(x=10, y=275)
-
-        self.confirm_password_entry = tk.Entry(self, border=1)
-        self.confirm_password_entry.place(x=125, y=275)
 
         self.captcha_label = tk.Label(self, text='Captcha')
         self.captcha_label.place(x=10, y=310)
