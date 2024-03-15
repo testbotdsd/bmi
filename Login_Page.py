21ff4cd0a1f13ea2536de1d6aaedd3d796fb85bb
ker <136089307+aikooox@users.noreply.github.com>
2024-03-14 21:48:53 -0700

    sadsad

--- Login_Page.py
+++ Login_Page.py
@@ -63,16 +63,53 @@ class Forgot_Password (tk.Frame):
         self.parent = master
         self.config(width=400, height=600)
 
-        self.login_bg = tk.Frame(self, bg='#7B6079', height=595, width=450)
-        self.login_bg.place(x=0, y=0)
+        self.forgot_password_label_bg = tk.Frame(self, bg='#7B6079', height=600, width=450)
+        self.forgot_password_label_bg.place(x=0, y=0)
 
-        self.welcome_label = tk.Label(self.login_bg, text="FORGOT PASSWORD", font=('Courier', 20, 'bold'), fg='white', 
-                                      bg='#7B6079')
-        self.welcome_label.place(x=95, y=40)
+        self.forgot_password_label = tk.Label(self.forgot_password_label_bg, text="Forgot Password", font=('Courier', 20, 'bold'), fg='white', bg='#7B6079')
+        self.forgot_password_label.place(x = 40, y = 30)
 
-        self.back_button = tk.Button(self.login_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'),
-                                     bg='#FFE9D6')
-        self.back_button.place(x=5, y=10)
+        # gmail num
+        self.gmail_num_label = tk.Label(self.forgot_password_label_bg, text="GMAIL NUMBER", font=('Courier', 15), fg='white', bg='#7B6079')
+        self.gmail_num_label.place(x = 100, y= 110)
+        
+        
+        self.gmail_num_entry = tk.Entry(self.forgot_password_label_bg, font=('Courier', 12))
+        self.gmail_num_entry.place(x= 100, y= 145)
+
+        #new pass
+        
+        self.new_password_label = tk.Label(self.forgot_password_label_bg, text="NEW PASSWORD", font=('Courier', 15), fg='white', bg='#7B6079')
+        self.new_password_label.place( x = 100, y= 190)
+        
+        
+        self.new_password_entry = tk.Entry(self.forgot_password_label_bg, font=('Courier', 12), show='*')
+        self.new_password_entry.place(x = 100, y= 225)
+
+        #conf pass
+        self.confirm_password_label = tk.Label(self.forgot_password_label_bg, text="CONFIRM PASSWORD", font=('Courier', 15), fg='white', bg='#7B6079')
+        self.confirm_password_label.place(x= 100, y= 270)
+        
+        
+        self.confirm_password_entry = tk.Entry(self.forgot_password_label_bg, font=('Courier', 12), show='*')
+        self.confirm_password_entry.place(x = 100, y = 305)
+
+
+        #verification
+        self.verification_label = tk.Label(self.forgot_password_label_bg, text="VERIFICATION", font=('Courier', 15), fg='white', bg='#7B6079')
+        self.verification_label.place(x= 100, y= 350)
+        
+        
+        self.verification_entry = tk.Entry(self.forgot_password_label_bg, font=('Courier', 12), show='*')
+        self.verification_entry.place(x = 100, y = 385)
+
+        #cont button
+        self.continue_button = tk.Button(self.forgot_password_label_bg, text="CONTINUE", font=('Courier', 12), fg='white', bg='#7B6079')
+        self.continue_button.place( x =150, y = 440)
+
+        #back button
+        self.back_button = tk.Button(self.forgot_password_label_bg, text="←", width=4, height=1, font=('Courier', 12, 'bold'), bg='#FFE9D6', command=self.go_to_login)
+        self.back_button.place(x= 5, y= 10)
 
     def go_to_login(self):
         self.parent.change_window('Login')
@@ -82,3 +119,5 @@ class Forgot_Password (tk.Frame):
 
     def on_return(self):
         pass
+
+
