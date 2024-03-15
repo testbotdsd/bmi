ccda16804c19c36d1e46040e15d539e3179b8f17
ker <136089307+aikooox@users.noreply.github.com>
2024-03-14 21:25:09 -0700

    dasdsa

--- Main.py
+++ Main.py
@@ -3,13 +3,14 @@ import Welcome_Page
 import Login_Page
 import BMI_Page
 import SIgnup_Page
-
+imp
 
 class MainWindow(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.wm_title("BMI CALCULATOR")     
 
+dasdasdsad  
         self.frames = {}
         self.frames ['Welcome_Page'] = Welcome_Page.Welcome(self)
 
