d4a1cb78120f5e39e1795e5aecb1675e3c75bab0
Shityhanta <pauljohnanicete506@gmail.com>
2024-03-16 16:38:29 +0800

    paultulog

--- Login_Page.py
+++ Login_Page.py
@@ -18,17 +18,17 @@ class Login(tk.Frame):
         self.username_label = tk.Label(self.login_bg, text="Username", font=('Courier', 15), fg='#EEEDEB', bg='#3C3633')
         self.username_label.place(x=81, y=185)
 
-        self.username_entry = tk.Entry(self.login_bg, border=1, width=23, font=('Courier', 15), bg='#59504b')
+        self.username_entry = tk.Entry(self.login_bg, border=1, width=23, font=('Courier', 15),fg='white', bg='#59504b')
         self.username_entry.place(x=81, y=215)
 
         self.pass_label = tk.Label(self.login_bg, text="Password", font=('Courier', 15), fg='#EEEDEB', bg='#3C3633')
         self.pass_label.place(x=81, y=265)
 
-        self.pass_entry = tk.Entry(self.login_bg, border=1, width=23, font=('Courier', 15), bg='#59504b', show='*')
+        self.pass_entry = tk.Entry(self.login_bg, border=1, width=19, font=('Courier', 15), fg='white', bg='#59504b', show='*')
         self.pass_entry.place(x=81, y=295)
 
-        self.toggle_password_button = tk.Button(self.login_bg, text="Show", font=('Courier', 10), bg='#3C3633', fg='#5e918e', command=self.toggle_password)
-        self.toggle_password_button.place(x=370, y=295)
+        self.toggle_password_button = tk.Button(self.login_bg, text="Show", font=('Courier', 10), bd=1, bg='#3C3633',fg='black', command=self.toggle_password)
+        self.toggle_password_button.place(x=322, y=296)
 
         self.login_button = CTkButton(self.login_bg, text='LOGIN', width=300,height=40,corner_radius=30, font=('Courier', 15, 'bold'), bg_color='#3C3633', fg_color='#E0CCBE', 
                                       text_color='black',command=self.validate_login)
@@ -38,10 +38,10 @@ class Login(tk.Frame):
                                                 bg='#3C3633', bd=0, command=self.go_to_forgot_password)
         self.forgot_password_button.place (x=166, y=322)
 
-        self.no_account_label = tk.Label(self, text="Don't have an account?", bg='#3C3633', font="Courier 10", foreground='#EEEDEB')
+        self.no_account_label = tk.Label(self.login_bg, text="Don't have an account?", bg='#3C3633', font="Courier 10", foreground='#EEEDEB')
         self.no_account_label.place(x=82, y=488)
 
-        self.sign_up_btn = tk.Button(self, text="Sign up now", bg='#3C3633', bd=0, font=('Courier', 10, 'bold'), 
+        self.sign_up_btn = tk.Button(self.login_bg, text="Sign up now", bg='#3C3633', bd=0, font=('Courier', 10, 'bold'), 
                                      command=self.go_to_signup, foreground='#5e918e')
         self.sign_up_btn.place(x=264, y=487)
 
@@ -50,11 +50,11 @@ class Login(tk.Frame):
     def toggle_password(self):
         if self.password_hidden:
             self.pass_entry.config(show='')
-            self.toggle_password_button.config(text="Hide")
+            self.toggle_password_button.config(text="Hide", bg='#3C3633', fg='white')
             self.password_hidden = False
         else:
             self.pass_entry.config(show='*')
-            self.toggle_password_button.config(text="Show")
+            self.toggle_password_button.config(text="Show", bg='white', fg='#3C3633')
             self.password_hidden = True
 
     def go_to_forgot_password(self):
