8256efeb7b84336366fcc741aa959bf421c773e9
Angelaineee <sampangangelaine@gmail.com>
2024-03-16 16:39:12 +0800

    sad

--- BMI_Page.py
+++ BMI_Page.py
@@ -1,60 +1,59 @@
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image, ImageTk
 
 class BMI(tk.Frame):
     def __init__ (self, master):
         tk.Frame.__init__  (self, master)
         self.parent = master
         self.config(width=400, height=600)
-        
-        # MAIN FRAME
-        self.main_frame = tk.Frame(self, bd=20, width=400, height=600, bg='#A7D0CD', relief='groove')
-        self.welcome_frame = tk.Frame(self.main_frame, bd=10, width=300, height=70, bg='grey', relief='raised') 
 
-        #Logout
-    #     self.Logout_Page = tk.Button (self.main_frame, text = 'Return', cursor='gumby', bg="grey", fg="white", font=("SimSun"), relief="raised", height=1, width=15, command=self.go_to_logout_page)
-    #     self.Logout_Page.place(x=5, y=5)
-        
-    # def go_to_logout_page(self):
-    #     self.master.change_window('Logout') 
-        
+        self.main_frame = tk.Frame(self, bg='#3C3633', height=600, width=450)
+
         # TOP FRAMES
-        self.frame_top_left = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='grey', relief='flat')
-        self.frame_top_right = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='grey', relief='flat')
+        self.frame_top_left = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='#747264', relief='flat')
+        self.frame_top_right = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='#747264', relief='flat')
         
         # BOTTOM FRAMES
-        self.frame_bottom_left = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='grey', relief='flat')
-        self.frame_bottom_right = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='grey', relief='flat')
+        self.frame_bottom_left = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='#747264', relief='flat')
+        self.frame_bottom_right = tk.Frame(self.main_frame, bd=10, width=140, height=80, bg='#747264', relief='flat')
         
         #Result Frame
-        self.frame_result = tk.Frame(self.main_frame, bd=10, width=300, height=70, bg='grey', relief='flat')
+        self.frame_result = tk.Frame(self.main_frame, bd=10, width=305, height=60, bg='#747264', relief='flat')
 
         self.main_frame.place(x=0, y=0)
-        self.welcome_frame.place(x=10, y=10)
-        self.frame_top_left.place(x=10, y=95)
-        self.frame_top_right.place(x=168, y=95)
-        self.frame_bottom_left.place(x=10, y=190)
-        self.frame_bottom_right.place(x=168, y=190)
-        self.frame_result.place(x=8, y=400)
+        self.frame_top_left.place(x=45, y=95)
+        self.frame_top_right.place(x=215, y=95)
+        self.frame_bottom_left.place(x=45, y=190)
+        self.frame_bottom_right.place(x=215, y=190)
+        self.frame_result.place(x=45, y=332)
 
-        self.welcome_label = tk.Label(self.welcome_frame, text="Welcome to BMI Calculator", bg='grey', font=('Courier', 12), foreground='white')
-        self.welcome_label.place(x=7, y=10)
 
-        self.submit_button = tk.Button (self.main_frame, text = 'Calculate BMI', cursor='gumby', bg="grey", fg="white", font=("SimSun"), relief="raised", height=1, width=15, command=self.calculate_BMI)
-        self.submit_button.place(x=172, y=285)
+        # self.create_account_bg = tk.Frame(self, bg='#DE8971', height=600, width=450)
+        # self.create_account_bg.place(x=0, y=0)
 
-        self.clear_button = tk.Button(self.main_frame, text = 'CLEAR', cursor='gumby', bg="grey", fg="white", font=("SimSun"), relief="raised", width=16, command = self.clear_button)
-        self.clear_button.place(x=10, y=285)
+        self.menu_img = Image.open("menu_icon.png")
+        self.menu_img = self.menu_img.resize((30, 30))
+        self.menu_icon = ImageTk.PhotoImage(self.menu_img)
+        self.menu_btn = tk.Button(self, image=self.menu_icon, highlightbackground='#DE8971', highlightcolor='#DE8971',
+                                  border=0, command=self.got_to_profile_page)
+        self.menu_btn.place(x=367, y=0)
+
+        self.welcome_label = tk.Label(self, text="BMI Calculator", bg='#3C3633', font=('Courier', 17, 'bold'), 
+                                      foreground='#E0CCBE')
+        self.welcome_label.place(x=100, y=10)
 
         # WEIGHT LABEL AND ENTRY
-        self.weight_kg_label = tk.Label(self.frame_top_left, text="Weight (kg)", bg='grey', font=("Perpetua", 13, 'bold'), foreground='white')
+        self.weight_kg_label = tk.Label(self.frame_top_left, text="Weight (kg)", bg='#747264', font=("Perpetua", 13, 'bold'), 
+                                        foreground='#E0CCBE')
         self.weight_kg_label.place(x=13,y=0)
-        self.weight_lb_label = tk.Label(self.frame_top_right, text="Weight (lb)", bg='grey', font=("Perpetua", 13, 'bold'), foreground='white')
+        self.weight_lb_label = tk.Label(self.frame_top_right, text="Weight (lb)", bg='#747264', font=("Perpetua", 13, 'bold'), 
+                                        foreground='#E0CCBE')
         self.weight_lb_label.place(x=14,y=0)
 
-        self.weight_kg_entry = tk.Entry(self.frame_top_left, width=10, font=("Peroetua", 10))
+        self.weight_kg_entry = tk.Entry(self.frame_top_left, width=10, font=("Peroetua", 10), bg='#E0CCBE')
         self.weight_kg_entry.place(x=15, y=30)
-        self.weight_lb_entry = tk.Entry(self.frame_top_right, width=10, font=("Peroetua", 10))
+        self.weight_lb_entry = tk.Entry(self.frame_top_right, width=10, font=("Peroetua", 10), bg='#E0CCBE')
         self.weight_lb_entry.place(x=15, y=30)
         
         self.weight_kg_entry.bind('<KeyRelease>', self.update_weight_lb)
@@ -62,31 +61,52 @@ class BMI(tk.Frame):
         
 
         # HEIGH LABEL AND ENTRY
-        self.height_cm_label = tk.Label(self.frame_bottom_left, text="Height (cm)", bg='grey', font=("Perpetua", 13, 'bold'), foreground='white')
+        self.height_cm_label = tk.Label(self.frame_bottom_left, text="Height (cm)", bg='#747264', font=("Perpetua", 13, 'bold'), 
+                                        foreground='#E0CCBE')
         self.height_cm_label.place(x=13,y=0)
-        self.height_m_label = tk.Label(self.frame_bottom_right, text="Height (m)", bg='grey', font=("Perpetua", 13, 'bold'), foreground='white')
+        self.height_m_label = tk.Label(self.frame_bottom_right, text="Height (m)", bg='#747264', font=("Perpetua", 13, 'bold'), 
+                                       foreground='#E0CCBE')
         self.height_m_label.place(x=18,y=0)
 
-        self.height_cm_entry = tk.Entry(self.frame_bottom_left, width=10, font=("Peroetua", 10))
+        self.height_cm_entry = tk.Entry(self.frame_bottom_left, width=10, font=("Peroetua", 10), bg='#E0CCBE')
         self.height_cm_entry.place(x=15, y=30)
-        self.height_m_entry = tk.Entry(self.frame_bottom_right, width=10, font=("Peroetua", 10))
+        self.height_m_entry = tk.Entry(self.frame_bottom_right, width=10, font=("Peroetua", 10), bg='#E0CCBE')
         self.height_m_entry.place(x=15, y=30)
+
+        self.submit_button = tk.Button (self.main_frame, text = 'Calculate BMI', cursor='gumby', bg='#E0CCBE', foreground='#3C3633', font=("Perpetua"), 
+                                        relief="raised", height=1, width=16, command=self.calculate_BMI)
+        self.submit_button.place(x=215, y=285)
+
+        self.clear_button = tk.Button(self.main_frame, text = 'Clear', cursor='gumby', bg='#E0CCBE', foreground='#3C3633', font=("Perpetua"), 
+                                        relief="raised", width=16, command = self.clear_button)
+        self.clear_button.place(x=45, y=285)
+
+        self.history_button = tk.Button (self.main_frame, text = 'View History', bg='#E0CCBE', foreground='#3C3633', font=("Perpetua"), 
+                                        relief="raised", height=1, width=16)
+        self.history_button.place(x=45, y=480)
+
+        self.save_button = tk.Button(self.main_frame, text = 'Save', bg='#E0CCBE', foreground='#3C3633', font=("Perpetua"), 
+                                        relief="raised", width=16)
+        self.save_button.place(x=215, y=480)
         
         # RESULT
-        self.result_label = tk.Label (self.frame_result, text='Your BMI is:',  bg='grey', font=("Perpetua", 13, 'bold'), foreground='white')
+        self.result_label = tk.Label (self.frame_result, text='Your BMI is:', bg='#747264', font=("Perpetua", 13, 'bold'), foreground='#E0CCBE')
         self.result_label.place (x =5, y = 6 )
         
-        self.result_entry = tk.Entry(self.frame_result, width= 20, font=("Peroetua", 10 ))
+        self.result_entry = tk.Entry(self.frame_result, width= 20, font=("Peroetua", 10 ), bg='#E0CCBE')
         self.result_entry.place (x = 120, y = 8)
         
         self.evaluation_result_label = None
-
         
         self.height_cm_entry.bind('<KeyRelease>', self.update_height_cm)
         self.height_m_entry.bind('<KeyRelease>', self.update_height_m)
 
-        self.logout_button = tk.Button(self.main_frame, text = 'Logout', bg="grey", fg="white", font=("SimSun"), relief="raised", width=16, command = self.go_to_welcome_page)
-        self.logout_button.place(x=10, y=525)
+        #Logout
+    #     self.Logout_Page = tk.Button (self.main_frame, text = 'Return', cursor='gumby', bg="grey", fg="white", font=("SimSun"), relief="raised", height=1, width=15, command=self.go_to_logout_page)
+    #     self.Logout_Page.place(x=5, y=5)
+        
+    # def go_to_logout_page(self):
+    #     self.master.change_window('Logout') 
     
   
     def update_weight_lb(self, event):
@@ -181,6 +201,9 @@ class BMI(tk.Frame):
             self.master.change_window('Welcome_Page')
         else:
             pass
+
+    def got_to_profile_page(self):
+        self.parent.change_window('Profile')
             
 
 
--- Profile_Page.py
+++ Profile_Page.py
@@ -6,11 +6,11 @@ class Profile(tk.Frame):
         self.parent = master
         self.config(width=400, height=600)
         
-        self.profile_bg = tk.Frame(self, bg='#7B6079', height=600, width=400)
+        self.profile_bg = tk.Frame(self, bg='#3C3633', height=600, width=400)
         self.profile_bg.place(x=0, y=0)
 
-        self.logout_button = tk.Button(self, text = 'Logout', bg="grey", fg="white", 
-                                       font=("SimSun"), relief="raised", width=16, command=self.go_to_main_page)
+        self.logout_button = tk.Button(self, text = 'Logout', font=('Courier', 15, 'bold'), bg='#E0CCBE', foreground='#3C3633', 
+                                        relief="raised", width=16, command=self.go_to_main_page)
         self.logout_button.place(x=10, y=10)
         
     def go_to_main_page(self):
