9faedb8c22b7a7f055dfb4b54836e0116abc887e
ker <136089307+aikooox@users.noreply.github.com>
2024-03-17 08:33:48 -0700

    last

--- SIgnup_Page.py
+++ SIgnup_Page.py
@@ -9,89 +9,89 @@ class Signup(tk.Frame):
         self.config(width=450, height=600)
 
         #WELCOME LABELS
-        self.create_account_bg = tk.Frame(self, bg='#7B6079', height=600, width=450)
+        self.create_account_bg = tk.Frame(self, bg='#3C3633', height=600, width=450)
         self.create_account_bg.place(x=0, y=0)
 
-        self.create_account_label = tk.Label(self, text='Create an Account', font=('Courier', 25, 'bold'), bg='#7B6079', foreground="white")
-        self.create_account_label.place(x=25, y=10)
+        self.create_account_label = tk.Label(self, text='Create an Account', font=('Courier', 25, 'bold'),  fg='#EEEDEB', bg='#3C3633')
+        self.create_account_label.place(x=55, y=10)
 
-        self.to_get_started_label = tk.Label(self, text='to get started', font=('Courier', 15, 'bold'),  bg='#7B6079', foreground="white")
-        self.to_get_started_label.place(x=118, y=40)
+        self.to_get_started_label = tk.Label(self, text='to get started', font=('Courier', 15, 'bold'),   fg='#EEEDEB', bg='#3C3633')
+        self.to_get_started_label.place(x=130, y=50)
 
         #INFORMATIONS
-        self.first_name_label = tk.Label(self, text='First Name', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.first_name_label.place(x=81, y=75)
+        self.first_name_label = tk.Label(self, text='First Name', font=('Courier', 13),  fg='#EEEDEB', bg='#3C3633')
+        self.first_name_label.place(x=45, y=120)
 
-        self.first_name_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23)
-        self.first_name_entry.place(x=81, y=99)
+        self.first_name_entry = tk.Entry(self, border=1, font=('Courier', 13), width=17, bg='#59504b')
+        self.first_name_entry.place(x=42, y=144)
 
-        self.last_name_label = tk.Label(self, text='Last Name', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.last_name_label.place(x=81, y=132)
+        self.last_name_label = tk.Label(self, text='Last Name', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.last_name_label.place(x=235, y=120)
 
-        self.last_name_entry = tk.Entry(self, border=1, font=('Courier', 13),width=23)
-        self.last_name_entry.place(x=81, y=156)
+        self.last_name_entry = tk.Entry(self, border=1, font=('Courier', 13),width=17, bg='#59504b')
+        self.last_name_entry.place(x=237, y=144)
 
-        self.gmail_label = tk.Label(self, text='Gmail', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.gmail_label.place(x=81, y=189)
+        self.gmail_label = tk.Label(self, text='Gmail', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.gmail_label.place(x=100, y=189)
 
-        self.gmail_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23)
-        self.gmail_entry.place(x=81, y=213)
+        self.gmail_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23, bg='#59504b')
+        self.gmail_entry.place(x=100, y=213)
 
-        self.username_label = tk.Label(self, text='Username', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.username_label.place(x=81, y=246)
+        self.username_label = tk.Label(self, text='Username', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.username_label.place(x=100, y=246)
 
-        self.username_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23)
-        self.username_entry.place(x=81, y=270)
+        self.username_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23, bg='#59504b')
+        self.username_entry.place(x=100, y=270)
 
-        self.password_label = tk.Label(self, text='Password', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.password_label.place(x=81, y=303)
+        self.password_label = tk.Label(self, text='Password', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.password_label.place(x=100, y=303)
 
-        self.password_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23)
-        self.password_entry.place(x=81, y=327)
+        self.password_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23, bg='#59504b')
+        self.password_entry.place(x=100, y=327)
 
-        self.confirm_password_label = tk.Label(self, text='Confirm Password', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.confirm_password_label.place(x=81, y=360)
+        self.confirm_password_label = tk.Label(self, text='Confirm Password', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.confirm_password_label.place(x=100, y=360)
 
-        self.confirm_password_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23)
-        self.confirm_password_entry.place(x=81, y=384)
+        self.confirm_password_entry = tk.Entry(self, border=1, font=('Courier', 13), width=23, bg='#59504b')
+        self.confirm_password_entry.place(x=100, y=384)
         
-        self.birthday_label = tk.Label(self, text = 'Birthday', font=('Courier', 13), fg='white', bg='#7B6079')
-        self.birthday_label.place(x=81, y=420) 
-        
+        self.birthday_label = tk.Label(self, text = 'Birthday', font=('Courier', 13), fg='#EEEDEB', bg='#3C3633')
+        self.birthday_label.place(x=90, y=420) 
+
         # Adding a Birthday Picker using a Combobox
         self.day_var = tk.StringVar()
         self.month_var = tk.StringVar()
         self.year_var = tk.StringVar()
 
-        self.day_label = tk.Label(self, text="Day", font=('Courier', 10), fg='white', bg='#7B6079')
+        self.day_label = tk.Label(self, text="Day", font=('Courier', 10),  fg='#EEEDEB', bg='#3C3633')
         self.day_label.place(x=81, y=444)
         self.day_combo = ttk.Combobox(self, textvariable=self.day_var, state='readonly', font=('Courier', 13), width=3)
         self.day_combo['values'] = [str(i) for i in range(1, 32)]
         self.day_combo.place(x=111, y=444)
         self.day_combo.current(0)
 
-        self.month_label = tk.Label(self, text="Month", font=('Courier', 10), fg='white', bg='#7B6079')
+        self.month_label = tk.Label(self, text="Month", font=('Courier', 10),  fg='#EEEDEB', bg='#3C3633')
         self.month_label.place(x=170, y=444)
         self.month_combo = ttk.Combobox(self, textvariable=self.month_var, state='readonly', font=('Courier', 13), width=3)
         self.month_combo['values'] = [str(i) for i in range(1, 13)]
         self.month_combo.place(x=220, y=444)
         self.month_combo.current(0)
 
-        self.year_label = tk.Label(self, text="Year", font=('Courier', 10), fg='white', bg='#7B6079')
+        self.year_label = tk.Label(self, text="Year", font=('Courier', 10),  fg='#EEEDEB', bg='#3C3633')
         self.year_label.place(x=280, y=444)
         self.year_combo = ttk.Combobox(self, textvariable=self.year_var, state='readonly', font=('Courier', 13), width=4)
         self.year_combo['values'] = [str(i) for i in range(1900, 2025)]
         self.year_combo.place(x=317, y=444)
         self.year_combo.current(0)
 
-        self.sign_up_button = tk.Button(self, text='Next', width=21, font=('Courier', 15, 'bold'),bg='#DE8971', command=self.go_to_Photo_page)
-        self.sign_up_button.place(x=65, y=517)
+        self.have_an_account_login_label = tk.Label(self, text='Already have an account?', bg='#3C3633', font="Courier 10", foreground='white')
+        self.have_an_account_login_label.place(x=100, y=560)
 
-        self.have_an_account_login_label = tk.Label(self, text='Already have an account?', bg='#7B6079', font="Courier 10", foreground='white')
-        self.have_an_account_login_label.place(x=68, y=560)
+        self.sign_up_button = tk.Button(self, text='Next', width=21, font=('Courier', 15, 'bold'),bg='#d3d3d3', command=self.go_to_Photo_page)
+        self.sign_up_button.place(x=85, y=517)
         
-        self.login_clickable = tk.Button(self, text="Login", bg='#7B6079', bd=0, font=('Courier', 10, 'bold'), foreground='#88f2ea', command=self.go_to_Login_Page)
-        self.login_clickable.place(x=275, y=559)
+        self.login_clickable = tk.Button(self, text="Login", fg='#EEEDEB', bg='#3C3633' , bd=0, font=('Courier', 10, 'bold'), foreground='#88f2ea', command=self.go_to_Login_Page)
+        self.login_clickable.place(x=295, y=559)
 
 
     def go_to_welcome_page(self):
@@ -106,6 +106,7 @@ class Signup(tk.Frame):
 
     def on_return(self):
         pass
+    
     
 class Photo (tk.Frame):
     def __init__(self, master):
