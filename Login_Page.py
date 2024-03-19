5a61031198302167a1441e157d72b852f4d6d714
Angelaineee <sampangangelaine@gmail.com>
2024-03-19 15:22:49 +0800

    sd

--- Login_Page.py
+++ Login_Page.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 from customtkinter import *
+import random
+import string
 
 class Login(tk.Frame):
     def __init__(self, master):
@@ -16,19 +18,19 @@ class Login(tk.Frame):
         self.welcome_label.place(x=95, y=40)
 
         self.username_label = tk.Label(self.login_bg, text="Username", font=('Courier', 15), fg='#EEEDEB', bg='#3C3633')
-        self.username_label.place(x=81, y=185)
+        self.username_label.place(x=81, y=134)
 
         self.username_entry = tk.Entry(self.login_bg, border=1, width=23, font=('Courier', 15),fg='white', bg='#59504b')
-        self.username_entry.place(x=81, y=215)
+        self.username_entry.place(x=81, y=168)
 
         self.pass_label = tk.Label(self.login_bg, text="Password", font=('Courier', 15), fg='#EEEDEB', bg='#3C3633')
-        self.pass_label.place(x=81, y=265)
+        self.pass_label.place(x=81, y=215)
 
         self.pass_entry = tk.Entry(self.login_bg, border=1, width=19, font=('Courier', 15), fg='white', bg='#59504b', show='*')
-        self.pass_entry.place(x=81, y=295)
+        self.pass_entry.place(x=81, y=249)
 
         self.toggle_password_button = tk.Button(self.login_bg, text="Show", font=('Courier', 10), bd=1, bg='white',fg='black', command=self.toggle_password)
-        self.toggle_password_button.place(x=322, y=296)
+        self.toggle_password_button.place(x=322, y=249)
 
         self.login_button = CTkButton(self.login_bg, text='LOGIN', width=300,height=40,corner_radius=30, font=('Courier', 15, 'bold'), bg_color='#3C3633', fg_color='#E0CCBE', 
                                         text_color='black',command=self.validate_login)
@@ -46,6 +48,20 @@ class Login(tk.Frame):
         self.sign_up_btn.place(x=264, y=487)
 
         self.password_hidden = True 
+        self.generate_captcha_button = tk.Button(self, text="Generate Captcha", font=('Courier', 12, 'bold'), bg='#d3d3d3', command=self.generate_captcha)
+        self.generate_captcha_button.place(x=110, y=290)
+
+        self.captcha_label = tk.Label(self, text='', font=("Arial", 12), width=10, bg='#59504b')
+        self.captcha_label.place(x=150, y=335)
+            
+        self.captcha = tk.Label(self, text="Enter Captcha", font=('Courier', 12, 'bold'), bg='#d3d3d3')
+        self.captcha.place(x=130, y=390)
+        self.captcha_input = tk.Entry(self, text="Enter Captcha here", width=20, bg='#59504b')
+        self.captcha_input.place(x=135, y=425)
+
+    def generate_captcha(self):
+        captcha = ''.join(random.choices(string.ascii_letters + string.digits, k=6,))
+        self.captcha_label.config(text=captcha)
 
     def toggle_password(self):
         if self.password_hidden:
